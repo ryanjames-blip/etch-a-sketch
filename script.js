57df30adbbfb13992c7024748743b62e5d2438c7
Ryan <ryandenbleyker@gmail.com>
2023-07-20 10:22:47 -0400

    TODO: add random color functionality

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const resizeButton = document.querySelector('#resize-button');
 resizeButton.addEventListener('click', resizeGrid);
 
 function drawGrid() {
+    clearGrid();
     for (let i = 0; i < units * units; i++) {
         const square = document.createElement('div');
         square.classList.add('grid-element');
@@ -23,7 +24,7 @@ function drawGrid() {
     }
     gridContainer.style = `grid-template-columns: repeat(${units}, 1fr);
                             grid-template-rows: repeat(${units}, 1fr)`;
-    // re-initialize gridElements and add event listener
+    // re-define gridElements and add event listener
     // to each grid element
     gridElements = document.querySelectorAll('.grid-element');
     gridElements.forEach(gridElement => gridElement.addEventListener('mouseover', colorElement));
@@ -31,6 +32,13 @@ function drawGrid() {
 
 function colorElement() {
     this.classList.add('grid-element-color');
+}
+
+function colorElementRand() {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    gridElement.style.color = `rbg(${red}, ${green}, ${blue})`;
 }
 
 function clearGrid() {
